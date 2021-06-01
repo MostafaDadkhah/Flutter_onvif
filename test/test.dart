d611acce1849dfa61213682c2a2048aa84bc4af9
Tom Sullivan <tom@msbit.com.au>
2021-06-01 21:11:15 +1000

    Add test for `parseSystemDateAndTime`

--- test/test.dart
+++ test/test.dart
@@ -1,8 +1,18 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:test/test.dart';
+
 import 'package:onvif/onvif.dart';
+import 'package:onvif/Repository/ParseData.dart';
 
 void main() {
-  test("Test Discovery", () {
+  test('Test Discovery', () {
     var onvif = ONVIF();
+  });
+  test('parseSystemDateAndTime', () async {
+    var input = await File('test/fixtures/GetSystemDateAndTimeResponse.xml').readAsString();
+    var output = await parseSystemDateAndTime(input);
+    expect(output, equals(DateTime(2010, 10, 29, 15, 52, 25)));
   });
 }
